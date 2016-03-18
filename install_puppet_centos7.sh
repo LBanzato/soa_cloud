03529076aee23e5680889b5ddcfa7d928a52fa6d
LBanzato <LBanzato@users.noreply.github.com>
2016-03-18 22:56:42 +0000

    improvements to the install script

--- install_puppet_centos7.sh
+++ install_puppet_centos7.sh
@@ -11,31 +11,38 @@ if [ "$EUID" -ne "0" ]; then
   exit 1
 fi
 
-if which puppet > /dev/null 2>&1; then
-  echo "Puppet is already installed."
-  exit 0
+# Install wget
+if which wget > /dev/null 2>&1; then
+  echo "wget already installed, moving on..."
+else
+  echo "Installing wget..."
+  yum install -y wget > /dev/null
 fi
 
-# Install wget
-echo "Installing wget..."
-yum install -y wget > /dev/null
+if which puppet > /dev/null 2>&1; then
+  echo "Puppet is already installed."
+else
+  # Install puppet labs repo
+  echo "Configuring PuppetLabs repo..."
+  repo_path=$(mktemp)
+  wget --output-document="${repo_path}" "${REPO_URL}" 2>/dev/null
+  rpm -i "${repo_path}" >/dev/null
 
+  # Install Puppet...
+  echo "Installing puppet"
+  yum install -y puppet > /dev/null
+  echo "Puppet installed!"
+fi
 
-# Install puppet labs repo
-echo "Configuring PuppetLabs repo..."
-repo_path=$(mktemp)
-wget --output-document="${repo_path}" "${REPO_URL}" 2>/dev/null
-rpm -i "${repo_path}" >/dev/null
+if which git > /dev/null 2>&1; then
+  echo "git is already installed."
+else
+  # Install git...
+  echo "Installing git"
+  yum install -y git > /dev/null
+fi
 
-# Install Puppet...
-echo "Installing puppet"
-yum install -y puppet > /dev/null
-
-# Install git...
-echo "Installing git"
-yum install -y git > /dev/null
-
+echo "Cloning git repository..."
 git clone https://github.com/LBanzato/soa_cloud.git
 cp -R ./soa_cloud/puppet/* $PUPPET_HOME
-
-echo "Puppet installed!"
+echo "Done!"
