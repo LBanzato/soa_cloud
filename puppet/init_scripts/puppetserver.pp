03e529d59d56bfcdeaf10811bb4966b04cda7771
Luigi Banzato <banzato.luigi@gmail.com>
2016-06-16 16:04:12 +0100

    Adding Firewall rules to allow puppe master access.

--- puppet/init_scripts/puppetserver.pp
+++ puppet/init_scripts/puppetserver.pp
@@ -1,12 +1,29 @@
 
-service { 'puppetserver':
-    ensure => running,
-    enabled => true,
+service { 'firewalld':
+    ensure => stopped,
+    enable => false
 }
 
-firewall { '100 allow Puppet master access':
-    port   => '8140',
+package { 'iptables-services':
+    ensure  => present,
+    require => Service['firewalld']
+}
+
+service { 'iptables':
+    ensure  => running,
+    enable  => true,
+    require => Package['iptables-services'],
+}
+
+firewall { '100 puppet master port access':
+    dport  => 8140,
     proto  => tcp,
     action => accept,
+    require => Service['iptables'],
+}
+
+service { 'puppetserver':
+    ensure => running,
+    enable => true,
 }
 
